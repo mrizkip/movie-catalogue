a5c0ee7a35e032ca1be0e4a9e9aa5d451cb7180b
Miftahul Rizki P <mrizkip@student.ub.ac.id>
2019-08-23 22:33:14 +0700

    penambahan recycler view adapter

--- app/src/main/java/me/mrizkip/moviecatalogue/movie/MovieAdapter.kt
+++ app/src/main/java/me/mrizkip/moviecatalogue/movie/MovieAdapter.kt
@@ -4,51 +4,45 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import me.mrizkip.moviecatalogue.R
 import me.mrizkip.moviecatalogue.model.Movie
+import kotlinx.android.synthetic.main.item_movie.view.*
 
-class MovieAdapter(private val context: Context) :
-    BaseAdapter() {
+class MovieAdapter(private val context: Context, private val movieList: ArrayList<Movie>, val clickListener: (Movie) -> Unit) :
+    RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
-    var movies: ArrayList<Movie> = arrayListOf()
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View
-        val viewHolder: ViewHolder
-        if (convertView == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
-            viewHolder = ViewHolder(view)
-            view.tag = viewHolder
-        } else {
-            view = convertView
-            viewHolder = convertView.tag as ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view).apply {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    clickListener(movieList[position])
+                }
+            }
         }
-        val movie = getItem(position) as Movie
-        viewHolder.bindItem(movie)
-        return view
     }
 
-    override fun getItem(position: Int): Any = movies[position]
+    override fun getItemCount(): Int = movieList.size
 
-    override fun getItemId(position: Int): Long = position.toLong()
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bindItem(movieList[position])
 
-    override fun getCount(): Int = movies.size
 
-    private class ViewHolder(itemView: View) {
-        private val imvPoster: ImageView = itemView.findViewById(R.id.itemMovie_imvPoster)
-        private val tvTitle: TextView = itemView.findViewById(R.id.itemMovie_tvTitle)
-        private val tvDesc: TextView = itemView.findViewById(R.id.itemMovie_tvDescription)
-        private val tvDate: TextView = itemView.findViewById(R.id.itemMovie_tvReleaseDate)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        private val imvPoster: ImageView = itemView.itemMovie_imvPoster
+//        private val tvTitle: TextView = itemView.findViewById(R.id.itemMovie_tvTitle)
+//        private val tvDesc: TextView = itemView.findViewById(R.id.itemMovie_tvDescription)
+//        private val tvDate: TextView = itemView.findViewById(R.id.itemMovie_tvReleaseDate)
 
         fun bindItem(movie: Movie) {
-            tvTitle.text = movie.title
-            tvDesc.text = movie.description
-            tvDate.text = movie.releaseDate
-            Picasso.get().load(movie.poster).resize(100, 140).centerCrop().into(imvPoster)
+            itemView.itemMovie_tvTitle.text = movie.title
+            itemView.itemMovie_tvDescription.text = movie.description
+            itemView.itemMovie_tvReleaseDate.text = movie.releaseDate
+            Picasso.get().load(movie.poster).resize(100, 140).centerCrop().into(itemView.itemMovie_imvPoster)
         }
     }
 }
